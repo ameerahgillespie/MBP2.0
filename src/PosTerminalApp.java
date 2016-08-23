a7f4011c29c29f106b4ed13ee21b89b6ea520eac
Meer Classwork <ameerahgillespie@gmail.com>
2016-08-23 09:18:45 -0400

    Changed classes

--- src/PosTerminalApp.java
+++ src/PosTerminalApp.java
@@ -1,7 +1,15 @@
 
+import java.util.Scanner;
 
 public class PosTerminalApp {
-	public static void	main(String[] args){
+	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
+		Inventory inventory = new Inventory();
+
+		System.out.println(inventory);
+		System.out.println("Please select your item of choice.");
 		
+		scan.close();
 	}
+	
 }
--- src/Product.java
+++ src/Product.java
@@ -1,4 +1,66 @@
 
+import java.util.Scanner;
 
 public class Product {
- }
+    private String name;
+    private String description;
+    private String category;
+    private double price;
+    
+    public Product(Scanner file) {
+        name = file.nextLine();
+        description = file.nextLine();
+        category = file.nextLine();
+        price = Double.parseDouble(file.nextLine());
+    }
+    
+    public Product(String name, String description, String category, double price) {
+        this.name = name;   
+        this.description = description;
+        this.category = category;
+        this.price = price;
+    }
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+    
+    public String toString() {
+        String ret = "";
+        
+        ret += "Name: " + name + "\n";
+        ret += "Description: " + description + "\n";
+        ret += "Category: " + category + "\n";
+        ret += "Price: " + price + "\n";
+        
+        return ret;
+    }
+}
