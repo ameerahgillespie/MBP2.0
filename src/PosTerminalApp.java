567e63cd799dc47dc7874b4a8c62b2e3b2dced27
Patrick <topolski.patrick@gmail.com>
2016-08-23 13:38:49 -0400

    worked on file reader

--- src/PosTerminalApp.java
+++ src/PosTerminalApp.java
@@ -1,13 +1,15 @@
 
 import java.util.Scanner;
 
-public class PosTerminalApp {
+public class PosTerminalApp extends FileReaderWriter {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner (System.in);
 		Inventory inventory = new Inventory();
 
 		System.out.println(inventory);
 		System.out.println("Please select your item of choice.");
+		
+//		file reader method --- fileReader();
 
 //public class PosTerminalApp {
 //	public static void	main(String[] args){
--- src/Product.java
+++ src/Product.java
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Scanner;
-
-public class Product {
-	String name;
-	String description;
-	double price;
-	String category;
-	
-	
-	public Product(String name, String description, double price, String category) {
-		super();
-		this.name = name;
-		this.description = description;
-		this.price = price;
-		this.category = category;
-	}
-
-	@Override
-	public String toString() {
-		return "Product [name=" + name + ", description=" + description + ", price=" + price + ", category=" + category
-				+ "]";
-	}
-=======
-
 import java.util.Scanner;
 
 public class Product {
@@ -48,74 +19,12 @@ public class Product {
         this.category = category;
         this.price = price;
     }
->>>>>>> 4c381bc7bd817226dcb61ace1da526472f1f5d3f
 
-	public String getName() {
-		return name;
-	}
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 4c381bc7bd817226dcb61ace1da526472f1f5d3f
-	public void setName(String name) {
-		this.name = name;
-	}
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 4c381bc7bd817226dcb61ace1da526472f1f5d3f
-	public String getDescription() {
-		return description;
-	}
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 4c381bc7bd817226dcb61ace1da526472f1f5d3f
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
-<<<<<<< HEAD
 
-	public double getPrice() {
-		return price;
-	}
-
-
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
-
-=======
->>>>>>> 4c381bc7bd817226dcb61ace1da526472f1f5d3f
-	public String getCategory() {
-		return category;
-	}
-
-<<<<<<< HEAD
-
-	public void setCategory(String category) {
-		this.category = category;
-	}
-	
-
-
-	public void fileWriter() throws IOException{
-		Scanner scan = new Scanner(System.in);
-		File path = new File("producsts");
-		int newProducts = scan.nextInt();
-		OutputStream outStream = new FileOutputStream(path);
-		outStream.write(newProducts);
-		outStream.close();
-		scan.close();
-	}
-
-}
-=======
 	public void setCategory(String category) {
 		this.category = category;
 	}
@@ -138,5 +47,9 @@ public class Product {
         
         return ret;
     }
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
 }
->>>>>>> 4c381bc7bd817226dcb61ace1da526472f1f5d3f
