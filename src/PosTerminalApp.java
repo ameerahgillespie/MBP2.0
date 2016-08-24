ea629b42e6c7270dab9568634f34790d7b3e21e9
Meer Classwork <ameerahgillespie@gmail.com>
2016-08-24 14:35:14 -0400

    re work code in main App

--- src/PosTerminalApp.java
+++ src/PosTerminalApp.java
@@ -1,169 +1,155 @@
 import java.io.IOException;
-import java.util.Scanner; 
-
+import java.util.Scanner;
 
 public class PosTerminalApp extends Inventory {
 	private static int paymentType;
-
 
 	public PosTerminalApp() throws IOException {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
+	public static void loadInventory(Inventory inventory) {
+	}
+	//public static void printInventory(){
 
-    public static void loadInventory(Inventory inventory){
-    }
-    
-    
-    //public static void printInventory(){
-    	
-    	
-    
-    //}
+	 //}
 
 	public static void main(String[] args) throws IOException {
-		Scanner scan = new Scanner (System.in);
+		Scanner scan = new Scanner(System.in);
 		Inventory inventory = new Inventory();
-	
-	    //load inventory
-	    loadInventory(inventory);
-	    
-	    int numProducts = Inventory.getProducts().size();
-	    int[] cart = new int[numProducts];
 
-        Boolean orderIncomplete = true;	
-	    // loop to get customer's order 
-	    do
-	    {
-	
-    	    //print inventory
-            printInventory();
-    
-            
-            //read a line of text from the user.
-            //That gets stored in the String input.
-            //Then firstChar is the first character of input converted to uppercase.
-            //The idea is to check for E or C
-            //so the user can enter numbers for the items
-            //or E to empty the cart
-            // make shopping cart
-            
-            System.out.println("Please select your item of choice.");
-            //add extra options
-            System.out.println("C to check out. E to empty your cart.");
-            String input = scan.nextLine(); 
-            String firstChar = input.substring(0,1).toUpperCase();
-            if (firstChar.equals("C")){
-                orderIncomplete=false;
-            } else if(firstChar.equals("E")){
-                //cart - new int[numProducts];
-            } else{
-                // make sure the input was a valid number from 1 to numProducts
-                int i = Integer.parseInt(input);
-                //later, handle if the user enters something that is not an integer and not E or C 
-                
-                
-                if (i<1 || i>numProducts){
-                //error message?
-                }
-                else{
-                
-                    // ask how many to add
-                    System.out.println("How many would you like?");
-                    int count = Integer.parseInt(scan.nextLine());
-                    //later, handle if the user enters something that is not valid
-                    cart[i-1] += count;
-                }
-            }
+		// load inventory
+		loadInventory(inventory);
 
-        } while (orderIncomplete);
+		int numProducts = Inventory.getProducts().size();
+		int[] cart = new int[numProducts];
 
+		Boolean orderIncomplete = true;
+		// loop to get customer's order
+		do {
 
-        //Check out
-        
-        //test if there are any items 
-        Boolean itemsExist = false;
-        for (int i = 0; i<numProducts; i++)
-            if (cart[i]>0){
-                itemsExist = true;
-            }
-        
-        if (itemsExist){
-        // compute and show the pretax total, tax, and total
-        
-        
-        // choose the payment type
-        System.out.println("(1)Cash (2)Check (3)Credit/Debit?");
-        setPaymentType(Integer.parseInt(scan.nextLine()));
-        
-        }else{ //no items in cart
-            System.out.println("Nothing this time? Have a nice day.");
-            scan.close();
-        }
+			// print inventory
+			printInventory();
+
+			// read a line of text from the user.
+			// That gets stored in the String input.
+			// Then firstChar is the first character of input converted to
+			// uppercase.
+			// The idea is to check for E or C
+			// so the user can enter numbers for the items
+			// or E to empty the cart
+			// make shopping cart
+
+			System.out.println("Please select your item of choice.");
+			// add extra options
+			System.out.println("C to check out. E to empty your cart.");
+			String input = scan.nextLine();
+			String firstChar = input.substring(0, 1).toUpperCase();
+			if (firstChar.equals("C")) {
+				orderIncomplete = false;
+			} else if (firstChar.equals("E")) {
+				// cart - new int[numProducts];
+			} else {
+				// make sure the input was a valid number from 1 to numProducts
+				int i = Integer.parseInt(input);
+				// later, handle if the user enters something that is not an
+				// integer and not E or C
+
+				if (i < 1 || i > numProducts) {
+					// error message?
+				} else {
+
+					// ask how many to add
+					System.out.println("How many would you like?");
+					int count = Integer.parseInt(scan.nextLine());
+					// later, handle if the user enters something that is not
+					// valid
+					cart[i - 1] += count;
+				}
+			}
+		} while (orderIncomplete);
+		// Check out
+		// test if there are any items
+		Boolean itemsExist = false;
+		for (int i = 0; i < numProducts; i++)
+			if (cart[i] > 0) {
+				itemsExist = true;
+			}
+		if (itemsExist) {
+			// compute and show the pretax total, tax, and total
+
+			// choose the payment type
+			System.out.println("(1)Cash (2)Check (3)Credit/Debit?");
+			setPaymentType(Integer.parseInt(scan.nextLine()));
+
+		} else { // no items in cart
+			System.out.println("Nothing this time? Have a nice day.");
+			scan.close();
+		}
 	}
-
 
 	private static void printInventory() {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	public static int getPaymentType() {
 		return paymentType;
 	}
 
-
 	public static void setPaymentType(int paymentType) {
 		PosTerminalApp.paymentType = paymentType;
-	}}
-        
+	}
+}
 
-//		//Welcome! Who do I have the pleasure 
-//		//print inventory
-//		System.out.println(inventory);
-//		//We want it to ask the user to pick an item
-//		System.out.println("Please select your item of choice.");
-//		//Then we want the user to enter the item they picked
-//		System.out.println("scanner.nextln");
-//		//do you want to add? y/n
-//		System.out.println("Would you like to add an item to your cart?");
-//		//if n then pull all items picked in shopping cart
-//		System.out.println("scanner.nextln");
-//		//get total price 
-//		System.out.println("get total price");
-//		//when finished ask payment method from user - cash, check or credit
-//		System.out.println("cash check or creeeedit?");
-//		System.out.println("scanner.nextln");
-//				//if cash pull from cash
-//		//if check pull from check
-//		//if credit pull from credit
-//	//	Payment.paymentType(scanstuffanswer);
+// //Welcome! Who do I have the pleasure
+// //print inventory
+// System.out.println(inventory);
+// //We want it to ask the user to pick an item
+// System.out.println("Please select your item of choice.");
+// //Then we want the user to enter the item they picked
+// System.out.println("scanner.nextln");
+// //do you want to add? y/n
+// System.out.println("Would you like to add an item to your cart?");
+// //if n then pull all items picked in shopping cart
+// System.out.println("scanner.nextln");
+// //get total price
+// System.out.println("get total price");
+// //when finished ask payment method from user - cash, check or credit
+// System.out.println("cash check or creeeedit?");
+// System.out.println("scanner.nextln");
+// //if cash pull from cash
+// //if check pull from check
+// //if credit pull from credit
+// // Payment.paymentType(scanstuffanswer);
 //
-////		ArrayList<BigDecimal> cart = new ArrayList<BigDecimal>();
-////		BigDecimal anumber= new BigDecimal (12.00).setScale(2, BigDecimal.ROUND_HALF_UP);
-////		BigDecimal anotherone = new BigDecimal (8.50).setScale(2, BigDecimal.ROUND_HALF_UP);
-////		BigDecimal meth = new BigDecimal (0.55).setScale(2, BigDecimal.ROUND_HALF_UP);
-////		cart.add(meth);
-////		cart.add(anotherone);
-////		cart.add(anumber);
-////		System.out.println(Checkout.cartTotal(cart));
+//// ArrayList<BigDecimal> cart = new ArrayList<BigDecimal>();
+//// BigDecimal anumber= new BigDecimal (12.00).setScale(2,
+// BigDecimal.ROUND_HALF_UP);
+//// BigDecimal anotherone = new BigDecimal (8.50).setScale(2,
+// BigDecimal.ROUND_HALF_UP);
+//// BigDecimal meth = new BigDecimal (0.55).setScale(2,
+// BigDecimal.ROUND_HALF_UP);
+//// cart.add(meth);
+//// cart.add(anotherone);
+//// cart.add(anumber);
+//// System.out.println(Checkout.cartTotal(cart));
 //
-//		//Payment.paymentType(scanstuffanswer);
-//		
-////		System.out.println(Checkout.cartTotal(cart));
-//		//Payment.paymentType(scanAnswer); <<<<<DO NOT REMOVE
-//		
-//		
-//		//Tell them their items will arrive in 4-7 business days (more like 7).
-//		System.out.println("bye felicia.");
-//		//Tell them thanks for shopping and come again!
-//	
-//		
-//		scan.close();
-//	}
+// //Payment.paymentType(scanstuffanswer);
 //
-//	
-//	
-//}
+//// System.out.println(Checkout.cartTotal(cart));
+// //Payment.paymentType(scanAnswer); <<<<<DO NOT REMOVE
+//
+//
+// //Tell them their items will arrive in 4-7 business days (more like 7).
+// System.out.println("bye felicia.");
+// //Tell them thanks for shopping and come again!
+//
+//
+// scan.close();
+// }
+//
+//
+//
+// }
